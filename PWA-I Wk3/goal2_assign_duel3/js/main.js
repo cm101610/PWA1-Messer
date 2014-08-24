4c0d680e2df92e695dfd4a2e8a04ca47de3619bd
Chris Messer <chrismesser24@yahoo.com>
2014-08-24 16:54:08 -0400

    Spelling correction

--- PWA-I Wk3/goal2_assign_duel3/js/main.js
+++ PWA-I Wk3/goal2_assign_duel3/js/main.js
@@ -167,7 +167,7 @@
         if(fighters[0].health < 1 && fighters[1].health < 1)
         {
             result = "You Both Die - GAME OVER!";
-        }else if(fighers[0].health < 1){
+        }else if(fighters[0].health < 1){
             result = fighters[1].name + " WINS!!!!";
         }else if(fighers[1].health < 1){
             result = fighters[0].name + " WINS!!!!";
